84e7125269adfe50067cf8f93f3dfc8ede854b96
Wojciech Sadowski <wojciech.sadowski@ruhr-uni-bochum.de>
2023-11-24 00:09:55 +0100

    fixed annoying issue

---
+++ Makefiles/functions.mk
@@ -0,0 +1,43 @@
+#
+# Helper variables
+#
+NC := \033[0m
+RED := \033[0;31m
+GREEN := \033[0;32m
+YELLOW := \033[33m
+
+OK := $(GREEN)[OK]$(NC)
+ERROR := $(RED)[ERROR]$(NC)
+WARNING := $(YELLOW)[WARNING]$(NC)
+
+
+# 
+# Functions
+#
+define check_if_folder_exists
+	@[ -d $(1) ] || ( printf "$(ERROR) Could not find $s!\n" $(1) && exit 1 )
+endef
+
+define msg_is_installed?
+	@printf "Checking if %s is installed... " $(1)
+endef
+
+define msg_found
+	@printf "$(OK) found %s\n" $(1)
+endef
+
+define msg_warn 
+	@printf "$(WARNING) %s\n" $(1)
+endef
+
+define clone_if_not_present
+	@printf "Checking if folder %s exists... " $(1)
+	@if [ -d $(1) ]; then \
+		printf "$(OK)\n"; \
+	else \
+		printf "cloning... " $(1); \
+		git clone -q $(3) $(2) $(1); \
+		printf "$(OK)\n"; \
+	fi
+endef
+
---
+++ Makefiles/trgt_help.mk
@@ -0,0 +1,7 @@
+help:
+	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(firstword $(MAKEFILE_LIST)) \
+	| sort \
+	| awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
+
+
+.DEFAULT_GOAL := help
